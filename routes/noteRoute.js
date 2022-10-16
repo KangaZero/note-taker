684bddfb715a3f35608314443f6591c49684d371
Samuel <samuelyongw@gmail.com>
2022-10-16 15:47:13 +1100

    Id route paramter added

--- routes/noteRoute.js
+++ routes/noteRoute.js
@@ -4,14 +4,50 @@ const path = require('path');
 //helpers
 const { readFromFile, writeTofile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
+const apiData = require('../db/db.json')
 //Router name
 const note = express.Router();
 
-note.get('/', (req, res) =>{
+note.get('/', (req, res) => {
     console.info(`${req.method} request for note`);
-    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
+    readFromFile(path.join(__dirname, '../db/db.json'))
+        .then((data) => {
+        res.json(JSON.parse(data));
+     
+        for (let i = 0; i < JSON.parse(data).length; i++) {
+            console.log((JSON.parse(data))[i].id)
+        }
+    
+    })
 });
 
+note.get('/id/:id', (req, res) => {
+    const id = req.params.id;
+    
+    console.info(`${req.method} request for id: ${id}`);
+
+    const findId = apiData.filter( (elem) => elem.id == id)
+    res.json(findId)
+    // readFromFile(path.join(__dirname, '../db/db.json'))
+    //     .then((data) => {
+            // const parsedData = JSON.parse(data)
+            // console.log(parsedData.length)
+            // res.json(parsedData)
+
+            // for (let i = 0; i < apiData.length; i++) {
+            //     console.log(apiData.length, req.params.id, apiData[i].id)
+            //         // console.log(apiData[])
+            //     if (req.params.id === apiData[i].id){
+            //       return res.json(apiData[i]);
+            //     } 
+            //     else {
+            //         return res.status(400).send("id not found!")
+            //     }
+                
+            // };
+
+    });
+// });
 
 note.post('/', (req, res) => {
     //TODO: create middleware for console.info
@@ -39,25 +75,26 @@ note.post('/', (req, res) => {
     }
 });
 
-// note.delete('/:id', (req, res) => {
-//     const id = req.params.id;
-//     fs.readFile('../db/db.json', (err, data) => {
-//         if (err) {
-//             throw err;
-//         } else {
-//             currentData = JSON.parse(data)
-//             //TODO def need to fix this up
-//             //filter and return objects without id of request
-//             for (let i = 0; i <  currentData.length; i++) {
-//                 //currentData.filter(() => currentData[i].id != id)
-//                 if (currentData[i].id == id) {
-//                     currentData.splice(currentData[i].id)
-//                    writeTofile('../db/db.json', currentData);
-//                 }  
-//             }      
-//         }
-//     });
-// });
+note.delete('/:id', (req, res) => {
+    
+    readFromFile(path.join(__dirname, '../db/db.json'), (err, data) => {
+        if (err) {
+            throw err;
+        } else {
+            const id = req.params.id;
+            const currentData = JSON.parse(data)
+            const newData = []
+
+            let removeCurrentData = currentData.map(() =>{
+                if (currentData.id != id){
+                    newData.push[currentData]
+                }
+            }) 
+            removeCurrentData();
+
+            writeTofile(newData, (path.join(__dirname, '../db/db.json')));
+        }
+    });
+});
 
 module.exports = note; 
-
